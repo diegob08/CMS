347ff05d37fa74e2e549c92d0eb879c255dd5299
diego <d.bernal91@gmail.com>
2018-03-26 19:51:55 -0400

    create movie admin page

--- admin/admin_movies.php
+++ admin/admin_movies.php
@@ -11,12 +11,10 @@ confirm_logged_in();
     <title>CMS Portal</title>
 </head>
 <body>
-<h1>Welcome Company Name to your admin page</h1>
+<h1>Welcome to Movies Dashboard</h1>
 <?php echo "<h2>Hi-{$_SESSION['user_name']}</h2>"; ?>
-<a href="admin_createuser.php">Create User</a><br>
-<a href="admin_edituser.php">Edit User</a><br>
-<a href="admin_deleteuser.php">Fired</a><br>
-<a href="admin_movies.php">Movies Dashboard</a><br>
+
+<a href="admin_index.php">Back to User Admin Page</a><br>
 <a href="phpscripts/caller.php?caller_id=logout">Sign Out</a>
 </body>
 </html>
